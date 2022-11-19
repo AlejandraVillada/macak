38ac4ba3219209b1f134a3722638305e135dfe0c
DcVillamil <dcvillamil9466@gmail.com>
2022-11-19 14:26:56 -0500

    cambios danna

---
+++ src/vistas/adopcion_apadrinamiento.php
@@ -0,0 +1,33 @@
+<style>
+        .barra_navegacion {
+            
+            background-color: rgba(var(--bs-back-primary)) !important;
+            position:absolute;
+            width:20%;
+            height: 448px;
+        }
+
+        .carta_fundaciones {
+            background-color: rgba(var(--bs-back-primary)) !important;
+        }
+    </style>
+
+    <!-- <section>
+        <div class="barra_navegacion">
+
+        </div>
+    </section> -->
+
+    <!-- Cartas de fundaciones -->
+    <section class="adopcion">
+        <div class="titulo">Adopcion y Apadrinamiento</div>
+        <hr>
+        <div id="adopcion_apadrinamiento">
+           
+        </div>
+    </section>
+
+    <script src="public/js/adopcion_apadrinamiento.js"></script>
+    <script>
+        $(document).ready(adopcion_apadrinamiento);
+    </script>
---
+++ src/vistas/fundaciones.php
@@ -0,0 +1,21 @@
+
+    <style>
+        .carta_fundaciones {
+            
+            background-color: rgba(var(--bs-back-primary)) !important;
+        }
+    </style>
+
+    <!-- Cartas de fundaciones -->
+    <section class="fundaciones">
+        <div class="titulo">FUNDACIONES ASOCIADAS A MACAK</div>
+        <hr>
+        <div id="fundacion">
+            
+        </div>
+    </section>
+
+    <script src="public/js/fundaciones.js"></script>
+    <script>
+        $(document).ready(fundaciones);
+    </script>
