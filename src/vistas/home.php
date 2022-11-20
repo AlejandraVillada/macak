1969e5180ad50672db74d559f5afd16ed3ec1604
Jorgeacero9 <61283804+Jorgeacero9@users.noreply.github.com>
2022-11-19 22:54:36 -0500

    Carrusel

--- src/vistas/home.php
+++ src/vistas/home.php
@@ -10,13 +10,13 @@ $_SESSION['title']='MACAK| Home ';
     <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <img src="public/img/carrousel2.png" class="d-block w-100" alt="...">
+                <img src="public/img/Macak_Carrusel1.gif" class="d-block w-100" alt="...">
             </div>
             <div class="carousel-item">
-                <img src="public/img/carrousel2.png" class="d-block w-100" alt="...">
+                <img src="public/img/Macak_Carrusel2.gif" class="d-block w-100" alt="...">
             </div>
             <div class="carousel-item">
-                <img src="public/img/carrousel2.png" class="d-block w-100" alt="...">
+                <img src="public/img/Macak_Carrusel3.jpg" class="d-block w-100" alt="...">
             </div>
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
