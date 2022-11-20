d05ef799e149288bd8e3f4467d1f37402048c031
Jorgeacero9 <61283804+Jorgeacero9@users.noreply.github.com>
2022-11-19 23:08:24 -0500

    Carrusel segunda parte

--- src/vistas/home.php
+++ src/vistas/home.php
@@ -16,7 +16,7 @@ $_SESSION['title']='MACAK| Home ';
                 <img src="public/img/Macak_Carrusel2.gif" class="d-block w-100" alt="...">
             </div>
             <div class="carousel-item">
-                <img src="public/img/Macak_Carrusel3.jpg" class="d-block w-100" alt="...">
+                <img src="public/img/Macak_Carrusel3.png" class="d-block w-100" alt="...">
             </div>
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
