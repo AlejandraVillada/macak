4167e52cd3332153d9ff9d4f53bf89914a8e54dc
AlejandraVillada <villadatorresmariaalejandra@gmail.com>
2022-11-26 00:02:58 -0500

    cartas

--- src/vistas/adopcion_apadrinamiento.php
+++ src/vistas/adopcion_apadrinamiento.php
@@ -10,6 +10,81 @@
     .carta_fundaciones {
         background-color: rgba(var(--bs-back-primary)) !important;
     }
+
+    .profile-card-4 {
+        max-width: 300px;
+        background-color: #FFF;
+        border-radius: 5px;
+        box-shadow: 0px 0px 25px rgba(0, 0, 0, 0.1);
+        overflow: hidden;
+        position: relative;
+        margin: 10px auto;
+        cursor: pointer;
+    }
+
+    .profile-card-4 img {
+        transition: all 0.25s linear;
+    }
+
+    .profile-card-4 .profile-content {
+        position: relative;
+        padding: 15px;
+        background-color: #FFF;
+    }
+
+    .profile-card-4 .profile-name {
+        font-weight: bold;
+        position: absolute;
+        left: 0px;
+        right: 0px;
+        top: -70px;
+        color: #FFF;
+        font-size: 17px;
+    }
+
+    .profile-card-4 .profile-name p {
+        font-weight: 600;
+        font-size: 13px;
+        letter-spacing: 1.5px;
+    }
+
+    .profile-card-4 .profile-description {
+        color: #777;
+        font-size: 12px;
+        padding: 10px;
+    }
+
+    .profile-card-4 .profile-overview {
+        padding: 15px 0px;
+    }
+
+    .profile-card-4 .profile-overview p {
+        font-size: 10px;
+        font-weight: 600;
+        color: #777;
+    }
+
+    .profile-card-4 .profile-overview h4 {
+        color: #273751;
+        font-weight: bold;
+    }
+
+    .profile-card-4 .profile-content::before {
+        content: "";
+        position: absolute;
+        height: 20px;
+        top: -10px;
+        left: 0px;
+        right: 0px;
+        background-color: #FFF;
+        z-index: 0;
+        transform: skewY(3deg);
+    }
+
+    .profile-card-4:hover img {
+        transform: rotate(5deg) scale(1.1, 1.1);
+        filter: brightness(110%);
+    }
 </style>
 
 <!-- <section>
@@ -29,38 +104,41 @@
     </div>
 
     <hr>
-    <div id="adopcion_apadrinamiento" class="row">
-        <!-- Modal -->
-        <div class="modal fade" id="detalle_mascota" tabindex="-1" aria-labelledby="detalle_mascota" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="nombre_mascota"></h5>
-                        <button type="button" class="btn-close cerrar_pet" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        <div id="contenido_modal">
-
-                        </div>
-                    </div>
-                    <div class="modal-footer">
-
+    <div class="container">
+        <div id="adopcion_apadrinamiento" class="row  text-center">
+        </div>
+    </div>
+    <!-- Modal -->
+    <div class="modal fade" id="detalle_mascota" tabindex="-1" aria-labelledby="detalle_mascota" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="nombre_mascota"></h5>
+                    <button type="button" class="btn-close cerrar_pet" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div id="contenido_modal">
 
                     </div>
-                    <div class="formulario_solicitudes">
-                        <form id="datos">
-                            <input type="hidden" name="id_user" id="id_user" value="1">
-                            <input type="hidden" name="id_fundacion" id="id_fundacion">
-                            <input type="hidden" name="id_mascota" id="id_mascota">
-                            <input type="hidden" name="estado" id="estado" value="0">
-                            <input type="hidden" name="accion" id="accion" value="guardar_solicitud">
-                        </form>
-                    </div>
+                </div>
+                <div class="modal-footer">
+
+
+                </div>
+                <div class="formulario_solicitudes">
+                    <form id="datos">
+                        <input type="hidden" name="id_user" id="id_user" value="1">
+                        <input type="hidden" name="id_fundacion" id="id_fundacion">
+                        <input type="hidden" name="id_mascota" id="id_mascota">
+                        <input type="hidden" name="estado" id="estado" value="0">
+                        <input type="hidden" name="accion" id="accion" value="guardar_solicitud">
+                    </form>
                 </div>
             </div>
         </div>
-
     </div>
+
+
 </section>
 
 <script src="public/js/adopcion_apadrinamiento.js"></script>
--- src/vistas/home.php
+++ src/vistas/home.php
@@ -140,7 +140,7 @@ $_SESSION['title'] = 'MACAK| Home ';
             <div class="slide m-3 text-center">
                 <img src="public/img/Fundacion_Razas_unicas.png" alt="" style="width: 200px ; border-radius:150px;">
             </div>
-            
+
         </div>
     </div>
 </section>
@@ -202,8 +202,51 @@ $_SESSION['title'] = 'MACAK| Home ';
     <h3>Mascotas</h3>
 </div>
 
-<section>
-    <div class="row" id="mascotas"></div>
-       
-</section>
 
+<div class="container">
+    <div id="mascotas" class="row  text-center">
+    </div>
+</div>
+
+<script>
+    $.ajax({
+        type: "get",
+        url: "src/controlador/controlador_adopcion_apadrinamiento.php",
+        data: {
+            accion: 'listar_home'
+        },
+        dataType: "json"
+    }).done(function(aa) {
+        console.log(aa);
+        $.each(aa.data, function(index, value) {
+            if (value.estado == 'publicado') {
+                var URL_imagen;
+                if (value.URL_imagen == '') {
+                    URL_imagen = 'public/img/logo.png';
+                } else {
+                    URL_imagen = 'public/img/animales/' + value.URL_imagen;
+                }
+                $("#mascotas").append(
+                    '<div class="profile-card-4 text-center col-lg-3 p-0">' +
+                    ' <img src="' + URL_imagen + '" class="img img-responsive" style="height:280px; width:auto;">' +
+                    '    <div class="profile-content bg-primary-plantilla">' +
+                    '        <div class="profile-description text-black">' +
+                    '            <h5>' + value.nombre + '</h5>' +
+                    '            <span>' + value.descripcion + '</span>' +
+                    '        </div>' +
+                    '        <div class="row">' +
+                    '            <div class="col-xs-4">' +
+                    '                <div class="profile-overview">' +
+                    // '                <button type="button" class="btn bg-secondary-plantilla vermas text-white" data-bs-toggle="modal" data-bs-target="#detalle_fundacion" data-codigo="' + value.id + '">Ver Más</button>' +
+                    '                </div>' +
+                    '            </div>' +
+                    '        </div>' +
+                    '    </div>' +
+                    '</div>'
+                );
+
+                // $("#mascotas").append("<div class='col-lg-12 col-sm-10 col-md-10 d-flex justify-content-center'><div class='col-lg-3 col-sm-10 col-md-10 m-2'><div class='row'><img src='" + value.URL_imagen + "' style='width: 200px;' alt='' srcset=''></div><div class='row'><h4>" + value.nombre + "</h4></div><div class='row'>" + value.descripcion + "</div></div></div>")
+            }
+        });
+    });
+</script>
