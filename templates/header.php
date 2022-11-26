2bee1de52edf9b826ed18ec3754a04443fa08be5
Jorgeacero9 <61283804+Jorgeacero9@users.noreply.github.com>
2022-11-26 10:21:15 -0500

    Favicon

--- templates/header.php
+++ templates/header.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>MACAK</title>
+    <link rel="icon" href="public/img/Favicon.png" type="image/png"/>
     <link rel="stylesheet" href="public/css/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <!-- sweetalert -->
@@ -12,6 +13,7 @@
 
     <!--Iconos Fontawsome -->
     <script src="https://kit.fontawesome.com/f2eddab7e4.js" crossorigin="anonymous"></script>
+  
 </head>
 
 <body>
