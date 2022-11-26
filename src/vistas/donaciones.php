faf6d6b9b93541ea3cfebacabac4bfa39817c4d6
Jorgeacero9 <61283804+Jorgeacero9@users.noreply.github.com>
2022-11-26 01:51:06 -0500

    Ajustes donaciones

--- src/vistas/donaciones.php
+++ src/vistas/donaciones.php
@@ -44,10 +44,11 @@
 <div class = "slider bg-primary-plantilla">
     <div class="text-center">
         <h2 class="text-white container">
-            
+            Tipo de donaciones
         </h2>
     </div>
 </div>
+<div class = "fl-row-content-wrap mt-4"><img src="public/img/Donacion.png" ></div>
 <div class = "fl-row-content-wrap mt-4"><img src="public/img/Donaciones.png" ></div>
 
 
