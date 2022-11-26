13a1be830f7e0f61b0c4dda425c58fac67664541
Jorgeacero9 <61283804+Jorgeacero9@users.noreply.github.com>
2022-11-25 23:16:49 -0500

    diseño donaciones

--- src/vistas/donaciones.php
+++ src/vistas/donaciones.php
@@ -1,4 +1,36 @@
+<style>
+    h2 {
+        color: white;
+        font-family: 'Permanent Marker', cursive;
+    }
 
+</style>
+
+<link href="https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap" rel="stylesheet">
+
+<div class = "slider bg-primary-plantilla">
+    <div class="titulo text-center mt-4">
+        <h2 class="text-white">¡Con tu aporte nos ayudas a salvar más peluditos!   </h2>
+    </div>
+</div>
+
+<div class = "fl-row-content-wrap mt-4"><img src="public/img/perros.png" ></div>
+
+<div class="text-center container-lg mt-4">
+	<p>
+        <span style="font-size: 20px;">
+            Te invitamos a ser parte de esta gran familia en donde podras apoyar a las fundaciones que tenemos para tí, para que conjuntamente podamos seguir construyendo una vida más justa y digna para estos peluditos.
+        </span>
+    </p>   
+</div>
+
+<div class = "slider bg-primary-plantilla">
+    <div class="text-center">
+        <h2 class="text-white container">
+            Recuerda que puedes hacer donaciones en 
+        </h2>
+    </div>
+</div>
 
 
 
