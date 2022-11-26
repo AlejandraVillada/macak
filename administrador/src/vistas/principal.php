eecd5c9f1a10e824e8621ccb46e385b80902127f
AlejandraVillada <villadatorresmariaalejandra@gmail.com>
2022-11-25 23:26:55 -0500

    modulo_admin

---
+++ administrador/src/vistas/principal.php
@@ -0,0 +1,53 @@
+<style>
+#contenido {
+    max-width: fit-content;
+    padding: 10px;
+}
+
+#contenido.active {
+    max-width: fit-content;
+}
+
+
+</style>
+<div class="p-3 ">
+    <div class="row">
+
+    </div>
+    <div class="row  m-0">
+        <div class="col ">
+            <div type="button" class="card card-light mt-2" data-toggle="modal" data-target="#modal_cliente"
+                style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title text-dark">Animales</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">Agregar Animales</h6>
+                    <div class="justify-content-center d-flex">
+                        <img src="../public/img/team.png" alt="">
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        <div class="col ">
+            <li class="list-unstyled p-0 m-0">
+                <a href="vistas/donaciones/reporte_1.php" class="listar nav-link align-middle px-0 m-0">
+                    <div class="card card-light" data-toggle="modal" style="width: 18rem;">
+                        <div class="card-body">
+                            <h5 class="card-title text-dark">Reportes Donaciones</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">Ver Reportes</h6>
+                            <div class="justify-content-center d-flex">
+                                <img src="../public/img/increase.png" alt="">
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </li>
+        </div>
+
+      
+    </div>
+    <div class="row">
+
+    </div>
+</div>
